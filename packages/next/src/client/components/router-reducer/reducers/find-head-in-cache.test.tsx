d0bed1cdf28adc3973a212b230571bbbac4424c4
Wanath <114429894+Wanath@users.noreply.github.com>
2025-01-08 17:59:49 +0000

    Initial commit

---
+++ packages/next/src/client/components/router-reducer/reducers/find-head-in-cache.test.tsx
@@ -0,0 +1,119 @@
+import React from 'react'
+import type { FlightRouterState } from '../../../../server/app-render/types'
+import type { CacheNode } from '../../../../shared/lib/app-router-context.shared-runtime'
+import { findHeadInCache } from './find-head-in-cache'
+
+describe('findHeadInCache', () => {
+  it('should find the head', () => {
+    const routerTree: FlightRouterState = [
+      '',
+      {
+        children: [
+          'linking',
+          {
+            children: [
+              'about',
+              {
+                children: ['', {}],
+              },
+            ],
+          },
+        ],
+      },
+      undefined,
+      undefined,
+      true,
+    ]
+
+    const cache: CacheNode = {
+      lazyData: null,
+      rsc: null,
+      prefetchRsc: null,
+      head: [null, null],
+      prefetchHead: null,
+      loading: null,
+      parallelRoutes: new Map([
+        [
+          'children',
+          new Map([
+            [
+              'linking',
+              {
+                lazyData: null,
+                rsc: null,
+                prefetchRsc: null,
+                head: [null, null],
+                prefetchHead: null,
+                loading: null,
+                parallelRoutes: new Map([
+                  [
+                    'children',
+                    new Map([
+                      [
+                        'about',
+                        {
+                          lazyData: null,
+                          head: [null, null],
+                          prefetchHead: null,
+                          loading: null,
+                          parallelRoutes: new Map([
+                            [
+                              'children',
+                              new Map([
+                                [
+                                  '',
+                                  {
+                                    lazyData: null,
+                                    rsc: null,
+                                    prefetchRsc: null,
+                                    prefetchHead: null,
+                                    loading: null,
+                                    parallelRoutes: new Map(),
+                                    head: [
+                                      null,
+                                      <>
+                                        <title>About page!</title>
+                                      </>,
+                                    ],
+                                  },
+                                ],
+                              ]),
+                            ],
+                          ]),
+                          rsc: null,
+                          prefetchRsc: null,
+                        },
+                      ],
+                      // TODO-APP: this segment should be preserved when creating the new cache
+                      // [
+                      //   '',
+                      //   {
+                      //     lazyData: null,
+                      //     rsc: <>Page</>,
+                      //     prefetchRsc: null,
+                      //     parallelRoutes: new Map(),
+                      //   },
+                      // ],
+                    ]),
+                  ],
+                ]),
+              },
+            ],
+          ]),
+        ],
+      ]),
+    }
+
+    const result = findHeadInCache(cache, routerTree[1])
+    expect(result).not.toBeNull()
+
+    const [cacheNode, key] = result!
+    expect(cacheNode.head[0]).toBe(null)
+    expect(cacheNode.head[1]).toMatchObject(
+      <>
+        <title>About page!</title>
+      </>
+    )
+    expect(key).toBe('/linking/about/')
+  })
+})
